6fcff9e9dbb662ab2a1b14518b1e5d0e9d7fa8f0
Larocceau <larocceau@gmail.com>
2021-12-09 22:06:02 +0100

    made day 9 part 1 more elegant

--- day9_smoke.py
+++ day9_smoke.py
@@ -1,36 +1,28 @@
 import sys
 
-def neighbours(cord, list):
-    locations = [(cord[0], cord[1]+1), (cord[0], cord[1]-1), (cord[0]-1, cord[1]),(cord[0]+1, cord[1])]
-    for location in locations:
-        if location[0] >= 0 and location[1] >=0:
-            try:
-                print(list[location[0]][location[1]])
-                yield list[location[0]][location[1]]
-            except IndexError:
-                pass
-
-
-
 attributes = sys.argv
 
 fileName  = attributes.pop(1)
 file = open(fileName)
-grid = []
 
-for line in file:
-    grid.append([int(item) for item in list(line)[:-1]])
+risk = 0
 
-for line in grid:
-    print(line)
 
-sumOfLows = 0
+data = [[{"depth": int(item), "lowPoint": True} for item in line[:-1]] for line in file]
 
-for line in range(len(grid)):
-    for position in range(len(grid[0])):
-        positionValue = grid[line][position];
-        if all([neighbour >positionValue for neighbour in neighbours((line, position), grid)]):
-            print("new low of %i at (%i, %i)" % (positionValue, line, position))
-            sumOfLows += positionValue +1
+for locations, topLocations  in zip(data,[[None]*len(data[0])] + data[:-1]):
 
-print("sum of lows :", sumOfLows)
+    for location, previousLocation, topLocation in zip( locations, [None] + locations[:-1], topLocations):
+        for adjecentLocation in [previousLocation, topLocation]:
+            if adjecentLocation:
+                if adjecentLocation["depth"] > location["depth"]:
+                    adjecentLocation["lowPoint"] = False
+                else:
+                    location["lowPoint"] = False
+
+
+for row in data:
+    for depth in row:
+        risk +=  depth["depth"]+1 if depth["lowPoint"] else 0
+
+print("risk: ", risk)
